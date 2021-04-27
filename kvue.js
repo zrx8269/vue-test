bba6359d40c1aa6d23f600ca444605220775ca04
I300351 <ruixue.zhang@sap.com>
2021-04-27 20:55:10 +0800

    add Observer and proxy

--- kvue.js
+++ kvue.js
@@ -27,6 +27,21 @@ function observe(obj) {
     // Object.keys(obj).forEach(key => defineReactive(obj, key, obj[key]));
 }
 
+// 设置一层代理，这样在UI层，可以直接访问data中的属性，this.prop, 而不用加this.$data.prop
+// 将$data中的key代理到KVue实例上
+function proxy(vm) {
+    Object.keys(vm.$data).forEach(key => {
+        Object.defineProperty(vm, key, {
+            get() {
+                return vm.$data[key]
+            },
+            set(v) {
+                vm.$data[key] = v;
+            }
+        })
+    })
+}
+
 class KVue{
     constructor(options) {
         this.$options = options;
@@ -34,8 +49,24 @@ class KVue{
 
         // 响应化处理
         observe(this.$data)
+
+        // 代理
+        proxy(this)
     }
 }
 
 // 每一个响应式对象，就有一个Observer实例
-class Observer {}
+// 主要作用是object的类型做不同的操作，这里没有展示
+class Observer {
+    constructor(value) {
+        this.value = value;
+
+        // 判断value是obj还是数组
+        this.walk(value)
+    }
+    walk(obj) {
+        Object.keys(obj).forEach(
+            key => defineReactive(obj, key, obj[key]
+        ));
+    }
+}
